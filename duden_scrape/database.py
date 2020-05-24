b156143adf80c5a8a92ca87dbfdde131a9ddf6ed
Johannes Schwarzbart <schwarzbartj@gmail.com>
2020-05-24 17:39:13 +0200

    catch error in drop() function: if table does not exist do nothing

--- duden_scrape/database.py
+++ duden_scrape/database.py
@@ -41,7 +41,10 @@ class DatabaseManager():
         )
     
     def drop_table(self, table_name):
-        self._execute(f"DROP TABLE {table_name};")
+        try:
+            self._execute(f"DROP TABLE {table_name};")
+        except sqlite3.OperationalError:
+            print(f"Table {table_name} does not exist and could not be dropped")
 
     def add(self, table_name, data):
         logger.debug(f"data: {data}", exc_info=True)
