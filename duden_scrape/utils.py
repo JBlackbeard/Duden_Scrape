bc2f01b88b39d4975d7e74671034d2b79eb4375d
Johannes Schwarzbart <schwarzbartj@gmail.com>
2020-05-25 11:22:27 +0200

    add helper functions to add word entry and meanings to db

--- duden_scrape/utils.py
+++ duden_scrape/utils.py
@@ -31,3 +31,35 @@ def load_word(word_url, base_url="https://www.duden.de",
         logger.exception(f"Unexpected response code for {url}: {source.status_code}")
 
     return Word(soup, url)
+
+def add_word_db(word_entry, db, url):
+    synonyme = word_entry.pop("synonyme") or None
+    db.add("wort", word_entry)
+    wort_id = db.select("id", "wort", {"url": "https://www.duden.de" + url}).fetchone()[0]
+    
+    if synonyme:
+        for synonym in synonyme.split(";"):
+            db.add("synonyme", {"synonyme": synonym, "wort_id": wort_id})
+
+    return wort_id
+
+def add_meanings_db(meanings, db, wort_id):
+    for bedeutung in meanings["bedeutungen"]:
+        bedeutung.update({"wort_id": wort_id})
+        beispiele = bedeutung.pop("beispiele") or []
+        wendungen = bedeutung.pop("wendungen_redensarten_sprichwoerter") or []
+        gebrauch = bedeutung.pop("gebrauch") or None
+        db.add("bedeutungen", bedeutung)
+        
+        bedeutung_id = db.select("max(id)", "bedeutungen").fetchone()[0]
+
+        for beispiel in beispiele:
+            db.add("beispiele", {"beispiel": beispiel, "bedeutungen_id": bedeutung_id})
+
+        for wendung in wendungen:
+            db.add("wendungen_redensarten_sprichwoerter", {"wendung_redensart_sprichwort": wendung, 
+            "bedeutungen_id": bedeutung_id})
+        
+        if gebrauch:
+            for geb in gebrauch.split(";"):
+                db.add("gebrauch", {"gebrauch": geb, "bedeutungen_id": bedeutung_id})
